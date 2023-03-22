621bd77dd1008102dc57629d09b9f1e8056a16b3
Thomas Hawley <thawley22@turing.edu>
2023-03-21 20:37:37 -0600

    test: Add test for /players feature

---
+++ spec/features/players/index_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe '/players', type: :feature do
+  describe 'as a visitor, when I visit the players index page' do
+    before(:each) do
+      @waterdeep = Campaign.create!(campaign_name: 'Waterdeep', dm_name: 'Thomas', first_dm: true, difficult_rating: 5)
+      @dragon_heist = Campaign.create!(campaign_name: 'Dragon Heist', dm_name: 'Lucy', first_dm: false, difficult_rating: 4)
+
+      @angel = @waterdeep.players.create!(player_name: 'Angel', character_name: 'Snow Devil', new_player: false, char_lvl: 1)
+      @margaret = @waterdeep.players.create!(player_name: 'Margaret', character_name: 'Marcus Arelious', new_player: false, char_lvl: 2)
+
+      @crow = @dragon_heist.players.create!(player_name: 'Crow', character_name: 'Nocturia', new_player: true, char_lvl: 2)
+      @alec = @dragon_heist.players.create!(player_name: 'Alec', character_name: 'Hockly', new_player: false, char_lvl: 4)
+    end
+
+    it 'I see each player in the system including the players attributes' do
+      visit '/players'
+
+      expect(page).to have_content(@angel.character_name)
+      expect(page).to have_content("Player Name: #{@angel.player_name}")
+      expect(page).to have_content("New Player: #{@angel.new_player}")
+      expect(page).to have_content("Character Level: #{@angel.char_lvl}")
+
+      expect(page).to have_content(@margaret.character_name)
+      expect(page).to have_content("Player Name: #{@margaret.player_name}")
+      expect(page).to have_content("New Player: #{@margaret.new_player}")
+      expect(page).to have_content("Character Level: #{@margaret.char_lvl}")
+
+      expect(page).to have_content(@crow.character_name)
+      expect(page).to have_content("Player Name: #{@crow.player_name}")
+      expect(page).to have_content("New Player: #{@crow.new_player}")
+      expect(page).to have_content("Character Level: #{@crow.char_lvl}")
+
+      expect(page).to have_content(@alec.character_name)
+      expect(page).to have_content("Player Name: #{@alec.player_name}")
+      expect(page).to have_content("New Player: #{@alec.new_player}")
+      expect(page).to have_content("Character Level: #{@alec.char_lvl}")
+    end
+  end
+end
