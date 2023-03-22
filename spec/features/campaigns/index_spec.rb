8e56a24cf964bc3eac6d4182c163148b93abc489
Thomas Hawley <thawley22@turing.edu>
2023-03-21 19:36:38 -0600

    test: Add feature test to campaign index

---
+++ spec/features/campaigns/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe '/campaigns', type: :feature do
+  describe 'as a visitor, when I visit the index page' do
+    before(:each) do
+      @waterdeep = Campaign.create!(campaign_name: 'Waterdeep', dm_name: 'Thomas', first_dm: true, difficult_rating: 5)
+      @dragon_heist = Campaign.create!(campaign_name: 'Dragon Heist', dm_name: 'Lucy', first_dm: false, difficult_rating: 4)
+    end
+    it 'see the name of each campaign record in the system' do
+
+      visit '/campaigns'
+
+      expect(page).to have_content(@waterdeep.name)
+      expect(page).to have_content(@dragon_heist.name)
+    end
+  end
+end
