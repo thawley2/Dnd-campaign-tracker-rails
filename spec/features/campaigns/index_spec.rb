9a3330b811d8d925815184cd6707a659b8b32a3b
Thomas Hawley <thawley22@turing.edu>
2023-03-21 21:00:12 -0600

    feat: Add /players#index feature

--- spec/features/campaigns/index_spec.rb
+++ spec/features/campaigns/index_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe '/campaigns', type: :feature do
-  describe 'as a visitor, when I visit the index page' do
+  describe 'as a visitor, when I visit the campaigns index page' do
     before(:each) do
       @waterdeep = Campaign.create!(campaign_name: 'Waterdeep', dm_name: 'Thomas', first_dm: true, difficult_rating: 5)
       @dragon_heist = Campaign.create!(campaign_name: 'Dragon Heist', dm_name: 'Lucy', first_dm: false, difficult_rating: 4)
