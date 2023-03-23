e2bf19b3b2467b0b52137bd1e4cea99c0c6e670d
Thomas Hawley <thawley22@turing.edu>
2023-03-23 16:56:21 -0600

    feat: Add link to /players on all pages

--- spec/features/campaigns/index_spec.rb
+++ spec/features/campaigns/index_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe '/campaigns', type: :feature do
   
   before(:each) do
     @waterdeep = Campaign.create!(campaign_name: 'Waterdeep', dm_name: 'Thomas', first_dm: true, difficult_rating: 5)
-    sleep(1)
     @dragon_heist = Campaign.create!(campaign_name: 'Dragon Heist', dm_name: 'Lucy', first_dm: false, difficult_rating: 4)
   end
 
