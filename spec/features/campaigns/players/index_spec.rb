0c6c2e1787469d434483b7f2424ce3b2e4f1b1da
Thomas Hawley <thawley22@turing.edu>
2023-03-23 20:03:33 -0600

    Remove save_and_open_page

--- spec/features/campaigns/players/index_spec.rb
+++ spec/features/campaigns/players/index_spec.rb
@@ -37,7 +37,7 @@ RSpec.describe '/campaigns/:campaign_id/players', type: :feature do
     it 'I see a link at the top of the page that takes me to the Campaigns index' do
 
       visit "/campaigns/#{@waterdeep.id}/players" 
-      save_and_open_page
+      # save_and_open_page
       click_link('Campaigns')
 
       expect(page).to have_current_path('/campaigns')
