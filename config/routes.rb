94ff123ee7a4550a638fab41e247e446d844a737
Thomas Hawley <thawley22@turing.edu>
2023-03-24 19:58:31 -0600

    feat: Add feature to update a player in their show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/campaigns', to: 'campaigns#index'
+  get '/campaigns/:id', to: 'campaigns#show'
   get '/campaigns/new', to: 'campaigns#new'
   post '/campaigns', to: 'campaigns#create'
   get '/campaigns/:id/edit', to: 'campaigns#edit'
   patch '/campaigns/:id', to: 'campaigns#update'
   get '/players', to: 'players#index'
   get '/players/:id', to: 'players#show'
-  get '/campaigns/:id', to: 'campaigns#show'
+  get '/players/:id/edit', to: 'players#edit'
+  patch '/players/:id', to: 'players#update'
   get '/campaigns/:campaign_id/players', to: 'campaigns/players#index'
   get '/campaigns/:campaign_id/players/new', to: 'campaigns/players#new'
   post '/campaigns/:campaign_id/players', to: 'campaigns/players#create'
