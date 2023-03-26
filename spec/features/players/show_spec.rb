a8014431bdd611f853ae3136bc6df1313e6cbc34
Thomas Hawley <thawley22@turing.edu>
2023-03-26 11:46:47 -0600

    test: Add feature test to delete player from show page

--- spec/features/players/show_spec.rb
+++ spec/features/players/show_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe '/players/:id', type: :feature do
 
   before(:each) do
     @waterdeep = Campaign.create!(campaign_name: 'Waterdeep', dm_name: 'Thomas', first_dm: true, difficult_rating: 5) 
-    @angel = @waterdeep.players.create!(player_name: 'Angel', character_name: 'Snow Devil', new_player: false, char_lvl: 2)
+    @angel = @waterdeep.players.create!(player_name: 'Angel', character_name: 'Snow Devil', new_player: true, char_lvl: 2)
   end
 
   describe 'As a visitor, when I visit the players show page' do
@@ -29,11 +29,22 @@ RSpec.describe '/players/:id', type: :feature do
     end
 
     it 'I see a link at the top of the page that takes me to the Campaigns index' do
+      visit '/players'
 
+      expect(page).to have_content(@angel.player_name)
+      
       visit "/players/#{@angel.id}" 
       click_link('Campaigns')
 
       expect(page).to have_current_path('/campaigns')
+    end
+
+    it 'I see a link to delete the player, on click player is deleted and redirect to player index page' do
+      visit "/players/#{@angel.id}"
+      click_button('Delete')
+
+      expect(current_path).to eq('/players')
+      expect(page).to_not have_content(@angel.player_name)
     end
   end
 end
